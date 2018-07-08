b6f35a2ca3dee4bea164108ca130b89d36af6b30
Cameron Moon <cameron@cameronmoon.com>
2018-07-08 18:58:14 +1000

    add snapshot to App component

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import basicComponentTests from '../basicComponentTests';
+import { shallow } from 'enzyme';
 import App from './App';
 
 basicComponentTests(<App />);
+
+it('renders the app with Results and Saved components', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper).toMatchSnapshot();
+});
